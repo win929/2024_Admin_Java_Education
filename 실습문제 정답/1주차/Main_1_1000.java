7e5ffdc361e0dd51756987415c66691e7a5e888e
Lee Eo Jin <win929@naver.com>
2024-02-25 20:34:55 +0900

    1week

---
+++ 실습문제 정답/1주차/Main_1_1000.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main_1_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a+b);
+
+        sc.close();
+    }
+}
---
+++ 실습문제 정답/1주차/Main_2_11382.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main_2_11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        System.out.println(a+b+c);
+
+        sc.close();
+    }
+}
